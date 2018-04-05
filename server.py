69d263e68abf7726734861a1e3d80c987550cbe0
moon1638 <oohtet96@gmail.com>
2018-04-05 23:30:08 +0630

    first commit

--- server.py
+++ server.py
@@ -1,98 +1,118 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template,request
 import json
 
-
+page_number =10
 w = json.load(open("worldl.json"))
 for c in w:
-	c['tld'] = c['tld'][1:]
+    c['tld'] = c['tld'][1:]
 page_size = 20
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def mainPage():
-	return render_template('index.html',
-		page_number=0,
-		page_size=page_size,
-		w = w[0:page_size])
+    l = len(w)
+    return render_template('index.html',
+                           w=w[0:page_size],page_number=0,page_size=page_size,l=l)
+
 
 @app.route('/begin/<b>')
 def beginPage(b):
-	bn = int(b)
-	return render_template('index.html',
-		w = w[bn:bn+page_size],
-		page_number = bn,
-		page_size = page_size
-		)
+    bn = int(b)
+    l = len(w)
+    return render_template('index.html',
+                           w=w[bn:bn + page_size],
+                           page_number=bn,
+                           page_size=page_size,l=l
+                           )
+
 
 @app.route('/continent/<a>')
 def continentPage(a):
-	cl = [c for c in w if c['continent']==a]
-	return render_template(
-		'continent.html',
-		length_of_cl = len(cl),
-		cl = cl,
-		a = a
-		)
+    cl = [c for c in w if c['continent'] == a]
+    return render_template(
+        'continent.html',
+        length_of_cl=len(cl),
+        cl=cl,
+        a=a
+    )
+
+@app.route('/createcountry/')
+def createcountry():
+    return render_template('createcountry.html')
 
 @app.route('/country/<i>')
 def countryPage(i):
-	return render_template(
-		'country.html',
-		c = w[int(i)])
+    return render_template(
+        'country.html',
+        c=w[int(i)])
+
 
 @app.route('/countryByName/<n>')
 def countryByNamePage(n):
-	c = None
-	for x in w:
-		if x['name'] == n:
-			c = x
-	return render_template(
-		'country.html',
-		c = c)
+    c = None
+    for x in w:
+        if x['name'] == n:
+            c = x
+    return render_template(
+        'country.html',
+        c=c)
+
+
+@app.route('/editCountryByName/<n>')
+def editCountryByNamePage(n):
+
+    c = None
+    for x in w:
+        if x['name'] == n:
+            c = x
+    return render_template(
+        'country-edit.html',
+        c=c)
+
+@app.route('/updateCountryByName')
+def updateCountryByNamePage():
+    n=request.args.get('name')
+    c = None
+
+    for x in w:
+        if x['name'] == n:
+            c = x
+    c['capital']=request.args.get('capital')
+    c['continent']=request.args.get('continent')
+    return render_template(
+        'country.html',
+        c=c)
+
+@app.route('/addNewCountry')
+def addNewCountry():
+    c={}
+    c['capital'] = request.args.get('capital')
+    c['name'] = request.args.get('name')
+    c['continent'] = request.args.get('continent')
+    c['area'] =int(request.args.get('area'))
+    c['population'] = int(request.args.get('population'))
+    c['gdp'] = int(request.args.get('gdp'))
+    c['tld']=request.args.get('tld')
+    w.append(c)
+    return render_template('country.html',c=c)
 
 @app.route('/delete/<n>')
+def deleteCountry(n):
+    i=0
+    for c in w:
+        if c['name']==n:
+            break
+        i=i+1
+    del w[i]
+    return render_template(
+        'index.html',
+        page_number=0,
+        page_size=page_size,
+        w=w[0:page_size])
 
-def deleteCountryPage(n):
-	i=0
-	for c in w:
-		if c['name'] == n:
-			break
-
-		i+=1
-
-	del w[i]
-	return render_template('index.html',
-		page_number=0,
-		page_size=page_size,
-		w = w[0:page_size])
-#all deleted country will be back on the list after restarting the server
-
-@app.route('/editcountryByName/<n>')
-def editcountryByNamePage(n):
-	c = None
-	for x in w:
-		if x['name'] == n:
-			c = x
-	return render_template(
-		'country-edit.html',
-		c = c)
-
-@app.route('/updatecountrybyname')
-def updatecountryByNamePage():
-	n=request.args.get('name')
-	c = None
-	for x in w:
-		if x['name'] == n:
-			c = x
-	c['capital']=request.args.get('capital')
-	c['continent']=request.args.get('continent')
-	return render_template(
-		'country.html',
-		c = c)
-
+    
 
 app.run(host='0.0.0.0', port=5638, debug=True)
-
-
-
 
